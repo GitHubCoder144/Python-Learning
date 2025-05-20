0691c09bb828c067cf61f42f7b79129a798ebaaa
Johan Babu <johanbabu@Johans-MacBook-Pro.local>
2025-05-20 10:23:51 -0700

    Started to learn how to print commands and connected it to inputs and output.

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Fedora Package Tester!")
+
+name=input("What is your name? ")
+package=input("Which Fedora package would you like to test?")
+
+print("Hello, " + name + "!")
+print("You are going to test the package:", package)
