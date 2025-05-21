f0a4da82d5f0613f470b28d97df0d0dba55ad135
GitHubCoder144 <johanbabu200@gmail.com>
2025-05-21 10:11:51 -0700

    added return values .py and learned how to get one of two outputs based on the input of a user.

---
+++ Unit One/Return_Values.py
@@ -0,0 +1,8 @@
+def greet(input):
+    if "hello" in input:
+        print("Hello, there!")
+    else:
+        print("I'm not sure what you mean.")
+
+greeting = greet("hello, computer?")
+print(greeting)
