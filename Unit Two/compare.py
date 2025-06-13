360f3161c5146877d228fc9bee2df443b11919e6
GitHubCoder144 <johanbabu200@gmail.com>
2025-06-12 20:18:20 -0700

    added if else etc items

---
+++ Unit Two/compare.py
@@ -0,0 +1,37 @@
+
+x = int(input("What is x?"))
+y= int(input("What is y?"))
+
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
+
+# Not Equal
+# !=
+
+# or
+# if x > y or x < y
+
+# Longer Version
+# if x < y:
+    #print("x is less than y")
+
+#elif x > y:
+    #print("x is greater than y")
+
+#else:
+    #print("x is equal to y")
+
+# We Can Also Use If But Slower for last two
+
+# if x < y:
+    # print("x is less than y")
+
+# if x > y:
+    # print("x is greater than y")
+
+# We Can also use elif for the last one if elif
+
+#elif x == y:
+    #print("x is equal to y")
