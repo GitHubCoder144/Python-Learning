8b8f8307dee2e1e90b652136a986beb3e12dc630
GitHubCoder144 <johanbabu200@gmail.com>
2025-06-21 16:17:45 -0700

    added Comparsion Operators Match and Parity

---
+++ Unit Two/parity.py
@@ -0,0 +1,21 @@
+def main():
+    x = int(input("What's x?: "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+
+
+def is_even(n):
+    return (n % 2 == 0)
+
+main()
+#return True if n % 2 == 0 else False
+
+#x = int(input("What's x?: "))
+#
+#if x % 2 == 0:
+#    print("x is even")
+#else:
+#    print("x is odd")
