076803532a819f8bd4dd9a00f128db42fa1838f2
GitHubCoder144 <johanbabu200@gmail.com>
2025-07-07 21:40:36 -0700

    activated lines 6-24 in cat.py from comments

--- Unit Three/cat.py
+++ Unit Three/cat.py
@@ -3,25 +3,25 @@
 # print("meow")
 
 # WHILE LOOPS
-# i = 0
-#while i < 3:
-#    print("meow")
-#    i += 1
+ i = 0
+while i < 3:
+    print("meow")
+    i += 1
 
 # LIST - for i in [0, 1, 2]
 
-#for _ in range(3):
-#    print("meow")
+for _ in range(3):
+    print("meow")
 
 # print("meow\n" * 3, end="")
 
-#while True:
-#    n = int(input("What's n?  "))
-#    if n > 0:
-#        break
+while True:
+    n = int(input("What's n?  "))
+    if n > 0:
+        break
 
-#for _ in range(n):
-#    print("meow")
+for _ in range(n):
+    print("meow")
 
 def main():
     number = get_number()
